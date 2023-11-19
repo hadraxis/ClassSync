2b1247b5758f340927f4f65f1eb9ad97e8ea2d8f
hadraxis <131629251+hadraxis@users.noreply.github.com>
2023-11-18 23:08:30 -0300

    ui temp

--- main.py
+++ main.py
@@ -1,39 +1,77 @@
 import tkinter as tk
 from tkinter import ttk
-from database import connect_database, get_students
-from schedule import organize_lessons, schedule_class
-from utils import authenticate_user, send_notification
+from students import students  # Replace this with a database import later
 
 def main():
-    # Backend Logic
-    connect_database()
-    user = authenticate_user()
-    students = get_students()
-    organize_lessons(students)
-    schedule_class(students)
-    send_notification("Classes have been scheduled and organized.")
-
-    # Tkinter GUI
     root = tk.Tk()
     root.title("Classync - Classroom Management")
+    root.geometry("1000x600")  # Adjusted for additional columns
 
-    # Add GUI components here
-    # Example: Label
-    label = ttk.Label(root, text="Welcome to Classync")
-    label.pack()
+    # Global style configuration
+    style = ttk.Style()
+    style.configure('Custom.TFrame', background='#f0f0f0')
+    style.configure('Custom.TButton', font=('Helvetica', 12), background='#0078D7')
+    style.configure('Custom.TLabel', background='#f0f0f0', font=('Helvetica', 10))
 
-    # Example: Button to Refresh Data
-    refresh_button = ttk.Button(root, text="Refresh Data", command=lambda: refresh_data(students))
-    refresh_button.pack()
+    # Header
+    header = ttk.Label(root, text="Classync - Student Management System", style='Custom.TLabel', font=('Helvetica', 16))
+    header.pack(side='top', fill='x', padx=10, pady=10)
 
-    # Start the GUI event loop
+    # Main Frame
+    main_frame = ttk.Frame(root, style='Custom.TFrame')
+    main_frame.pack(side='top', fill='both', expand=True)
+
+    # Treeview for Student Data
+    tree = setup_treeview(main_frame)
+    tree.pack(side='left', fill='both', expand=True)
+
+    # Scrollbar for Treeview
+    setup_scrollbar(main_frame, tree)
+
+    # Control Panel for Buttons
+    control_panel = setup_control_panel(root)
+
+    # Status Bar
+    status_bar = ttk.Label(root, text="Ready", style='Custom.TLabel')
+    status_bar.pack(side='bottom', fill='x')
+
+    # Populate Treeview with Data
+    populate_treeview(tree, students)
+
     root.mainloop()
 
-def refresh_data(students):
-    # Function to refresh student data on the UI
-    # This is where you will integrate backend logic with the UI
-    for student in students:
-        print(student['nome_do_aluno'])  # Example action
+def setup_treeview(main_frame):
+    columns = ('Name', 'Warm Up', 'Lesson', 'Activity', 'Homework', 'Details')
+    tree = ttk.Treeview(main_frame, columns=columns, show='headings')
+    for col in columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=150, minwidth=100)
+    return tree
+
+def setup_scrollbar(main_frame, tree):
+    scrollbar = ttk.Scrollbar(main_frame, orient='vertical', command=tree.yview)
+    scrollbar.pack(side='right', fill='y')
+    tree.configure(yscroll=scrollbar.set)
+
+def setup_control_panel(root):
+    control_panel = ttk.Frame(root, style='Custom.TFrame')
+    control_panel.pack(side='top', fill='x')
+    btn_refresh = ttk.Button(control_panel, text="Refresh Data", style='Custom.TButton', command=refresh_data)
+    btn_refresh.pack(side='left', padx=10, pady=10)
+    return control_panel
+
+def refresh_data():
+    # Function to refresh data
+    pass
+
+def populate_treeview(tree, student_data):
+    for student in student_data:
+        tree.insert('', tk.END, values=(student['nome_do_aluno'], 
+                                        "Placeholder Warm Up", 
+                                        student['lesson_of_the_day'], 
+                                        "Placeholder Activity", 
+                                        "Placeholder Homework",
+                                        "Additional Details"))
 
 if __name__ == "__main__":
     main()
--- schedule.py
+++ schedule.py
@@ -1,14 +1,28 @@
 # schedule.py
 
 def organize_lessons(students):
-    # Example logic for organizing lessons
+    """
+    Organizes lessons for each student based on certain criteria.
+    For example, even-numbered lessons might be input sessions, and odd-numbered lessons might be output sessions.
+    """
     for student in students:
-        if student["lesson_of_the_day"] % 2 == 0:
-            print(f"{student['nome_do_aluno']}'s lesson is an input session.")
-        else:
-            print(f"{student['nome_do_aluno']}'s lesson is an output session.")
+        try:
+            # Convert lesson_of_the_day to an integer
+            lesson_number = int(student["lesson_of_the_day"])
+            
+            if lesson_number % 2 == 0:
+                print(f"{student['nome_do_aluno']}'s lesson is an input session.")
+            else:
+                print(f"{student['nome_do_aluno']}'s lesson is an output session.")
+        except ValueError:
+            # Handle cases where conversion to integer fails
+            print(f"Error: Invalid lesson number for {student['nome_do_aluno']}.")
 
 def schedule_class(students):
-    # Example logic for scheduling classes
+    """
+    Schedules classes for each student. This function is a placeholder and currently
+    just prints a scheduling message for each student. Replace this with your actual
+    scheduling logic.
+    """
     for student in students:
-        print(f"Scheduling class for {student['nome_do_aluno']} on {student['lesson_of_the_day']}.")
+        print(f"Scheduling class for {student['nome_do_aluno']}.")
